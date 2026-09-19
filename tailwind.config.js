e86d4670c57c380f66c3ca4f6d2a122329013d81
Rustys90 <s90.vfx@gmail.com>
2026-09-19 15:59:52 +0530

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { screens: { xl: "1100px" } } },
+  plugins: [],
+};
